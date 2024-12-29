e6ee328a622b5b846cc2a83ff21ef1d00fa4eaaf
Eli Bautista <it.elibautista@gmail.com>
2024-12-29 16:27:17 +0800

    added descriptions for new projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
-import project1 from "../assets/projects/project-1.png"; 
+import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.png";
 import project5 from "../assets/projects/project-5.png";
 import project6 from "../assets/projects/project-6.jpg";
-import project7 from "../assets/projects/project-7.jpg"; 
+import project7 from "../assets/projects/project-7.jpg";
 
 export const HERO_CONTENT = `I'm a student passionate about learning different technologies. Here, I share my journey of trying out various projects to improve my skills in web development and other technologies. Each project helps me learn and grow.`;
 
@@ -49,31 +49,36 @@ export const PROJECTS = [
   {
     title: "NiyogHub Blog",
     image: project3,
-    description: "-",
+    description:
+      "NiyogHub Blog is a coconut news blog app built with React, Vite, and Tailwind CSS, and Express for the backend and MongoDB for the database. It features user authentication with cookie-based token storage, registration notifications, and password resets. Users can create, edit, and delete blog posts, providing an easy way to manage and share content.",
     technologies: ["React", "Tailwind", "Express.js", "MongoDB"],
   },
   {
     title: "NoteKeeper",
     image: project4,
-    description: "-",
+    description:
+      "NoteKeeper is a note-taking app built with React and Vite, styled with Tailwind CSS. It utilizes Django for the backend and SQLite3 for the database. It supports user authentication, email notifications, password resets, and CRUD operations for notes. The app features a responsive design, text editing, and a RESTful API for seamless communication.",
     technologies: ["React", "Tailwind", "Django", "SQLite3"],
   },
   {
     title: "Elliana coffee + sweets",
     image: project5,
-    description: "-",
+    description:
+      "Experience the charm of Elliana coffee + sweets through this website. Built with HTML, CSS, and JavaScript, this project showcases the cozy ambiance the coffee shop.",
     technologies: ["HTML", "CSS", "JavaScript"],
   },
   {
     title: "EspressoExpress",
     image: project6,
-    description: "-",
+    description:
+      "EspressoExpress is a mobile app crafted with Dart and Flutter. Explore the world of coffee with this dynamic application that brings the coffee experience to your fingertips.",
     technologies: ["Dart", "Flutter"],
   },
   {
     title: "Student Information System",
     image: project7,
-    description: "-",
+    description:
+      "Simplify education management with this desktop app in Python, PyQt6, and SQLite. It features automatic email alerts for student registrations and enables administrators to send messages directly to registered students.",
     technologies: ["Python", "PyQt6", "SQL"],
   },
 ];
